e15c414b9c3fa38c0ef83be13afe1bb4e1b05858
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2023-12-01 19:22:08 +0100

    Fix 2022

--- Advent-of-Code-2022/11/classes.py
+++ Advent-of-Code-2022/11/classes.py
@@ -26,14 +26,83 @@ class Monkey:
             res = self.opN(self.op1) + self.opN(self.op2)
         elif self.op==1:
             res = self.opN(self.op1) * self.opN(self.op2)
+        return res
 
-    
+    def inspect(self):
+        new_value = self.calculate()
+        self.inspected += 1
+        head = self.objects.pop(0)
+        return new_value
+
+    def chill(self, obj):
+        return int(obj/3)
 
     def to_string(self):
         res = "Monkey {0}: \n Starting Items: {1} \n Operation: new = {2} {3} {4} \n Test: divible by {5} \n \t If true: throw to monkey {6} \n \t If false: throw to monkey {7}"
 
         return res.format(self.id, self.objects, self.op1, self.op, self.op2, self.test, self.monkey_true, self.monkey_false)
     
+    def throw_obj(self, value):
+        if value % self.test == 0:
+            return self.monkey_true
+        else: return self.monkey_false
+
+    def opN(self, val):
+        if val==0: return self.objects[0]
+        else : return val
+
+
+class Monkey2:
+    id = 0
+    objects = []
+    op1 = 0 # 0 = old
+    op = 0 # 0 = + | 1 = *
+    op2 = 0 # 0 = old
+    test = 1
+    monkey_true = 0
+    monkey_false = 0
+    inspected = 0
+
+    def __init__(self, id: int, objs: list, op1: int, op: int, op2: int, test : int, monkey_true : int, monkey_false : int):
+        self.id = id
+        self.inspected = 0
+        self.op = op
+        self.op1 = op1
+        self.op2 = op2
+        self.objects = objs
+        self.test = test
+        self.monkey_true = monkey_true
+        self.monkey_false = monkey_false
+
+    def calculate(self):
+        res = 0
+        if self.op==0:
+            res = self.opN(self.op1) + self.opN(self.op2)
+        elif self.op==1:
+            res = self.opN(self.op1) * self.opN(self.op2)
+        return res
+
+    def inspect(self):
+        new_value = self.calculate()
+        self.inspected += 1
+        head = self.objects.pop(0)
+        return new_value
+
+    def chill(self, obj):
+        return int(obj/3)
+    
+    def chill_mod(self, obj, module):
+        return obj % module
+
+    def to_string(self):
+        res = "Monkey {0}: \n Starting Items: {1} \n Operation: new = {2} {3} {4} \n Test: divible by {5} \n \t If true: throw to monkey {6} \n \t If false: throw to monkey {7}"
+
+        return res.format(self.id, self.objects, self.op1, self.op, self.op2, self.test, self.monkey_true, self.monkey_false)
+    
+    def throw_obj(self, value):
+        if value % self.test == 0:
+            return self.monkey_true
+        else: return self.monkey_false
 
     def opN(self, val):
         if val==0: return self.objects[0]
--- Advent-of-Code-2022/11/p2.py
+++ Advent-of-Code-2022/11/p2.py
@@ -1,10 +1,16 @@
-from classes import Monkey
+from classes import Monkey2
 import sys
 rounds = 20
 n = len(sys.argv)
 if n > 1:
     rounds = int(sys.argv[1])
     print("{0} Rounds".format(sys.argv[1]))
+
+def multiply(elements : list):
+    base = 1
+    for e in elements:
+        base *= e
+    return base
 
 def turn_into_monkey(id : int, objs : str, operation: str, test : int , m_t : int, m_f : int):
     obj_list = [int(i.strip()) for i in objs.split(",") if i != ""]
@@ -13,7 +19,7 @@ def turn_into_monkey(id : int, objs : str, operation: str, test : int , m_t : in
     if opers[2] == "old": opers[2] = 0
     if opers[1] == "+": opers[1] = 0
     else: opers[1] = 1
-    return Monkey(id, obj_list,int(opers[0]), opers[1],int(opers[2]),test, m_t, m_f)
+    return Monkey2(id, obj_list,int(opers[0]), opers[1],int(opers[2]),test, m_t, m_f)
 
 with open("input.txt", "r") as file:
     monkey_list = []
@@ -46,6 +52,9 @@ with open("input.txt", "r") as file:
                 m_false = int(line[line.find("y")+1:].strip())
     monkey_list.append(turn_into_monkey(id, objs, operation, test, m_true, m_false))
 
+    total_module = multiply(i.test for i in monkey_list)  
+    print(total_module)
+
     # Phases   
     for i in range(rounds):
         for m in monkey_list:
@@ -53,14 +62,14 @@ with open("input.txt", "r") as file:
             for index in range(objs_len):
                 #phase 1: Inspection
                 obj = m.inspect()
-                obj = m.chill(obj)
                 #phase 2:
-                monkey_list[m.throw_obj(obj)].objects.append(obj)
+                obj = m.chill_mod(obj, total_module)
                 #phase 3:
+                monkey_list[m.throw_obj(obj)].objects.append(obj)
 
 
+    for i in monkey_list: print(i.to_string())
     # Monkey Business
     monkey_list.sort(key=lambda x: x.inspected, reverse=True)
     monkey_business = monkey_list[0].inspected * monkey_list[1].inspected
-    for i in monkey_list: print(i.to_string())
     print(monkey_business)
